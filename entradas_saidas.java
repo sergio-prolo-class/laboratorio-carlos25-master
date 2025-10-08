f2ff68f98b16e5d2a8bb0756a074aefeeeea86b2
carlos25-master <carlos.e19@aluno.ifsc.edu.br>
2025-10-07 21:57:25 -0300

    Update entradas_saidas.java

--- entradas_saidas.java
+++ entradas_saidas.java
@@ -1,14 +1,14 @@
-import java.util.Scanner
+import java.util.Scanner;
 
-public class LerLinha
-import java.util. Scanner;
+public class entradas_saidas.java {
+  public static void main(String[] args) { 
+    Scanner sc = new Scanner(System.in);
 
-public static void main(String[] args) { Scanner sc = new Scanner(System.in);
+    System.out.print("Digite algo: "); String entrada = sc.nextLine(); // Lê toda a linha de texto
 
-System.out.print("Digite algo: "); String entrada = sc.nextLine(); // Lê toda a linha de texto
+    System.out.println ("Você digitou: " + entrada);
 
-System.out.println ("Você digitou: " + entrada);
+    sc.close();
 
-sc.close();
-
+  }
 }
