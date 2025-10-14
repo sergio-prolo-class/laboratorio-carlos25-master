de4cc4d7ac1eb9454407918b43d079bca60e5831
carlos25-master <carlos.e19@aluno.ifsc.edu.br>
2025-10-14 19:21:23 -0300

    Create comandoIF.java

---
+++ comandoIF.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+public class comandoIF {
+public static void main(String[] args) {
+  int idade, tempo;
+  Scanner sc = new Scanner(System.in);
+  System.out.print("Entre com idade: ");
+  idade = sc.next
+  String entrada = sc.nextLine(); // Lê toda a linha de texto
+  System.out.println("Você digitou: " + entrada);
+  sc.close();
+  }
+}
