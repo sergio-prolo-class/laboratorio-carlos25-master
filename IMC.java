d9b34496203539002c45f5e1a6c7f2c875f93373
carlos25-master <carlos.e19@aluno.ifsc.edu.br>
2025-10-14 19:35:07 -0300

    Update IMC.java
    adicionado filtros  caso os valores do usuario não possam ser zero.

--- IMC.java
+++ IMC.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
+public class IMC {
+public static void main(String[] args) {
+  int peso, altura,valor;
+  Scanner sc = new Scanner(System.in); // criando objeto do tipo scaner
+  
+  System.out.print("Digite seu peso: ");
+  peso = sc.nextInt(); // le um inteiro
+  if(peso<0){
+  sc.close();
+    return;
+  }
+  
+  System.out.print("Digite sua altura:");
+  altura = sc.nextInt(); // le um inteiro
+   if(altura<0){     
+    sc.close();
+    return;
+}
+  valor = (altura*altura)/peso;
+  System.out.print("Seu IMC é ";+valor);
 
+  
+}
